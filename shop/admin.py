bb69e4af84e56cb80650a7f61495a85b5883390d
John <X00214104@mytudublin.ie>
2024-11-27 15:47:57 +0000

    Added Category and Product models

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Category, Product
 
-# Register your models here.
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'description']
+    search_fields = ['name']  
+
+admin.site.register(Category, CategoryAdmin)
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'price', 'description', 'category', 'stock', 'available', 'created', 'updated']
+    list_filter = ['available', 'created', 'updated', 'category'] 
+    list_editable = ['price', 'stock', 'available']
+    search_fields = ['name', 'description']  
+    list_per_page = 20
+
+admin.site.register(Product, ProductAdmin)
