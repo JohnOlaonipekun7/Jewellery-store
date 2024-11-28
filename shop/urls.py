b9e7ff7a15f43dc55e8013fbd96552c5b20805d0
John <X00214104@mytudublin.ie>
2024-11-28 12:37:53 +0000

    set up product templates and temporate display

--- shop/urls.py
+++ shop/urls.py
@@ -4,5 +4,7 @@ from . import views
 app_name = 'shop'
 
 urlpatterns = [
-    path('', views.prod_list, name = 'all_products'),
+    path('', views.all_products, name='all_products'),
+    path('category/<uuid:category_id>/', views.products_by_category, name='products_by_category'),
+    path('category/<uuid:category_id>/product/<uuid:product_id>/', views.product_detail, name='product_detail'),
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,4 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Category, Product
 
-def prod_list(request):
-    return render(request, 'shop/prod_list.html')
+
+def all_products(request):
+    products = Product.objects.filter(available=True)
+    return render(request, 'shop/prod_list.html', {'products': products})
+
+
+def products_by_category(request, category_id):
+    category = get_object_or_404(Category, id=category_id)
+    products = Product.objects.filter(category=category, available=True)
+    return render(request, 'shop/prod_list.html', {'category': category, 'products': products})
+
+
+def product_detail(request, category_id, product_id):
+    category = get_object_or_404(Category, id=category_id)
+    product = get_object_or_404(Product, id=product_id, category=category)
+    return render(request, 'shop/product.html', {'category': category, 'product': product})
