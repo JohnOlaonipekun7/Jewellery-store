8e4c6f3c5bd05fe33513ccbdc347c203e49c1a8c
John <X00214104@mytudublin.ie>
2024-11-29 18:32:54 +0000

    Paginator added

--- shop/views.py
+++ shop/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Category, Product
-
+from django.core.paginator import Paginator, EmptyPage, InvalidPage
 
 def all_products(request):
     products = Product.objects.filter(available=True)
+    
+    paginator = Paginator(products, 6) 
+    try:
+        page = int(request.GET.get('page', '1'))
+    except:
+        page = 1
+
+    try:
+        products = paginator.page(page)
+    except (EmptyPage, InvalidPage):
+        products = paginator.page(paginator.num_pages) 
     return render(request, 'shop/prod_list.html', {'products': products})
 
 
@@ -17,3 +28,4 @@ def product_detail(request, category_id, product_id):
     category = get_object_or_404(Category, id=category_id)
     product = get_object_or_404(Product, id=product_id, category=category)
     return render(request, 'shop/product.html', {'category': category, 'product': product})
+
